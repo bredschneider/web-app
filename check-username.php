7af537d8d4b49b716b8a3a4b447228606f315c2e
jbred034 <bred0014@algoqnuinlive.com>
2012-07-30 13:06:27 -0400

    Added a DOCBlock

--- check-username.php
+++ check-username.php
@@ -1,5 +1,16 @@
 <?php 
-
+/**
+* Checks to see if the username can be found within the database
+*
+*@author Jessica Bredschneider <jessica.bredschneider@gmail.com>
+*@copyright 2012 Jessica Bredschneider
+*@license BSD-3-Clause <http://github.com/jbred034/web-app/NEW-BSD-LICENSE.txt>
+*@version 1.0.0
+*@package Sweaty Betty
+*
+*
+*
+*/
 require_once 'includes/db.php';
 
 $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING) ;
